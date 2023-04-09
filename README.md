25e0094dbd35b3896a0480b1299f1eef25c3eec1
tda29 <tda29@sfu.ca>
2023-04-09 12:31:05 -0700

    minor layout adjustments

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # AI Companion
-IAT222 Final Project \
+IAT222 Final Project /
 
 note: I need to change the name of the directory when I am done 
 
 sources:
+https://www.w3schools.com/react/default.asp
 https://www.youtube.com/watch?v=R1S_NhKkvGA
--- recipebook/src/index.js
+++ recipebook/src/index.js
@@ -11,6 +11,121 @@ root.render(
   </React.StrictMode>
 );
 
+//Chat Dialogue 
+
+let state = {}; //keep track of choices
+
+const textElement = document.getElementById('text');
+const optionButtonsElement = document.getElementById('response-options');
+
+function startGame()
+{
+  state = {};
+  showTextNode(1);
+}
+
+function showTextNode(textNodeIndex)
+{
+  //display current conversation text
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
+  textElement.innerText = textNode.text;
+  //remove the preset 4 options set when defining the format 
+  while(optionButtonsElement.firstChild)
+  {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+  textNode.options.forEach(option => {
+    if(showOption(option))
+    {
+      const button = document.createElement('button');
+      button.innerText = option.text;
+      button.classList.add('btn');
+      button.addEventListener('click', () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  })
+}
+
+function showOption(option)
+{
+  //if there is no required state or our required state is our current state, show the options
+  if(option.requiredState == null || option.requiredState(state))
+  {
+    return true;
+  }
+  return;
+}
+
+function selectOption(option)
+{
+  const nextTextNodeId = option.nextText; 
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNodeId);
+}
+
+//conversation tree
+const textNodes = 
+[
+  {
+    id: 1,
+    text: "Hello there, Nice to meet you :)",
+    options:
+    [
+      {
+        text: "Hello",
+        nextText: 2
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    text: "What is your favourite season?",
+    options:
+    [
+      {
+        text: "Autumn",
+        setState: {autumn: true}, //might be uneccesary, change background to season
+        background: "orange", //just a tester 
+        nextText: 3
+      },
+      {
+        text: "Winter",
+        setState: {winter: true},
+        nextText: 3
+      },
+      {
+        text: "Spring",
+        setState: {spring: true},
+        nextText: 3
+      },
+      {
+        text: "Summer",
+        setState: {summer: true},
+        nextText: 3
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: "wipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwipwip\nwipwip",
+    options:
+    [
+      {
+        text: "aw"
+      },
+      {
+        text: "aw"
+      },
+      {
+        text: "aw"
+      }
+    ]
+  }
+]
+
+startGame();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
